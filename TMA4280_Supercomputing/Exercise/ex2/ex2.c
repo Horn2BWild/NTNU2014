27791a29b8a168e81c5737358963a35ee51d88fb
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-01-21 15:40:11 +0100

    added function prototypes

--- TMA4280_Supercomputing/Exercise/ex2/ex2.c
+++ TMA4280_Supercomputing/Exercise/ex2/ex2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Function Prototypes
+double* add(const double* vector1, const double* vector2, int size);
+double* multiply(const double* vector1, const double* vector2, int size);
+
+//const static declarations
 const double A[][3] = {{0.3, 0.4, 0.3},
                          {0.7, 0.1, 0.2},
                         {0.5, 0.5, 0.0}};
@@ -8,10 +13,11 @@ const double A[][3] = {{0.3, 0.4, 0.3},
 const double b[3] = {1.0, 1.0, 1.0};
 const double a[3] = {0.1, 0.2, 0.3};
 
+//main
 int main(int argc, char** argv)
 {
   double y[3] = {0}; //sum vector y
-  double x[3] = {0]; //sum vector x
+  double x[3] = {0}; //sum vector x
   double Ab[3]={0}; //A multiplied by b
   double gb[3]={0}; //b multiplied by gamma
   int i=0, j=0;
@@ -60,7 +66,7 @@ int main(int argc, char** argv)
   return EXIT_SUCCESS;
 }
 
-double* add(double* vector1, double* vector2, int size)
+double* add(const double* vector1, const double* vector2, int size)
 {
   double* result = (double*)malloc(size*sizeof(double));
   int i=0;
@@ -73,7 +79,7 @@ double* add(double* vector1, double* vector2, int size)
   return result;
 }
 
-double* multiply(double* vector1, double* vector2, int size)
+double* multiply(const double* vector1, const double* vector2, int size)
 {
   double* result = (double*)malloc(size*sizeof(double));
   int i=0;
