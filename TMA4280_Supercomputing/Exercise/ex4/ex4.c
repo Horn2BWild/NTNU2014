809dee1a1a6b1cac94ed2f3f72009af6e28db5d6
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-02-04 10:49:54 +0100

    pi() defined

---
+++ TMA4280_Supercomputing/Exercise/ex4/ex4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "common.h"
+
+int main(int argc, char** argv)
+{
+  int vectorlength=0;
+
+  Vector v=createVector(vectorlength);
+  return EXIT_SUCCESS;
+}
+
+double pi()
+{
+  return 4*atan(1);
+}
