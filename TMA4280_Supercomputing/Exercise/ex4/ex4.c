0e1145a77e84de7c049bb15b6fcc654115b6f597
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-02-04 11:05:52 +0100

    calculation of (S-Sn), serial version

--- TMA4280_Supercomputing/Exercise/ex4/ex4.c
+++ TMA4280_Supercomputing/Exercise/ex4/ex4.c
@@ -7,14 +7,28 @@ int main(int argc, char** argv)
 {
   int vectorlength=pow(2,14);
   double S=pow(pi(),2)/6;
+  double Sn=0.0;
   Vector v=createVector(vectorlength);
   int i=0;
+  int j=0;
+  double diff=0.0;
 
 //calculate vector elements
   for(i=0; i<vectorlength; i++)
   {
     v->data[i]=1/pow(i,2);
+    Sn += v->data[i];
+
+    for(j=3; j<=14; j++)
+    {
+      if(i==pow(2,j))
+      {
+        diff=S-Sn;
+        fprintf(stdout, "k=%d:\t%d", j, diff);
+      }
+    }
   }
+
 
   return EXIT_SUCCESS;
 }
