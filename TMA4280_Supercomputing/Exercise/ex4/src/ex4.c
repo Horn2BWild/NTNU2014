0ea413966651608512978b87f83ea5e26a5b3a7a
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-02-04 14:32:32 +0100

    generalizing program for variable boundaries

--- TMA4280_Supercomputing/Exercise/ex4/src/ex4.c
+++ TMA4280_Supercomputing/Exercise/ex4/src/ex4.c
@@ -16,17 +16,35 @@
 //function prototypes
 double mathPi();
 
+//! \usage: ex4 <lowerbound> <upperbound>
+//e.g. 3..14
 int main(int argc, char** argv)
-{ double startTime=WallTime();
+{ 
+  double startTime=WallTime();
   double endTime=0.0;
-  int vectorlength=pow(2,14); //maximum vector length
   double S=pow(mathPi(),2)/6; //reference value S
   double Sn=0.0; //approximated Sn
   Vector v=createVector(vectorlength); //storage vector for sum elements
   int i=0;
   int j=0;
   double diff=0.0; //difference S-Sn
+  int rank=0;
+  int size=0;
+  int klower=0;
+  int kuppper=0;
 
+  if(argc<3)
+  {
+    fprintf("\nusage: ex4 <lowerbound> <upperbound>\nexiting...\n\n");
+    return EXIT_FAILURE;
+  }
+
+  klower=atoi(argv[1]);
+  kupper=atoi(argv[2]);
+
+  int vectorlength=pow(2,kupper); //maximum vector length
+
+  init_app(argc, argv, &rank, &size);
   /*---DECOMMENT FOR DEBUGGING PURPOSES---
   fprintf(stdout, "------precalculated values------\n");
   fprintf(stdout, "-- PI: %f\n", mathPi());
@@ -35,6 +53,7 @@ int main(int argc, char** argv)
   */
 
 //calculate vector elements
+  #pragma omp parallel for schedule(guided,1) reduction(+:Sn)
   for(i=1; i<vectorlength; i++)
   {
     v->data[i]=1/pow(i,2);
@@ -50,7 +69,7 @@ int main(int argc, char** argv)
     }
   */
 
-    for(j=3; j<=14; j++)
+    for(j=klower; j<=kupper; j++)
     {
       if(i==pow(2,j)-1)
       {
@@ -71,6 +90,7 @@ int main(int argc, char** argv)
 
   fprintf(stdout, "total run time: %f\n\n", endTime-startTime);
 
+  close_app();
   return EXIT_SUCCESS;
 }
 
