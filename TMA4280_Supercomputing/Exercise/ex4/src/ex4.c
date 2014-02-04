5c7a9f703a49a39fc195c4045a58196aedc0992d
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-02-04 17:16:14 +0100

    generated MPI file structure

--- TMA4280_Supercomputing/Exercise/ex4/src/ex4.c
+++ TMA4280_Supercomputing/Exercise/ex4/src/ex4.c
@@ -1,10 +1,10 @@
-/** 
-  * Author: Andreas J. Hoermer
-  * Email: andreas (at) hoermer.at 
-  * 
-  * Filename: ex4.c
-  * LastChange: 04.02.2014
- **/
+/**
+* Author: Andreas J. Hoermer
+* Email: andreas (at) hoermer.at
+*
+* Filename: ex4.c
+* LastChange: 04.02.2014
+**/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +19,7 @@ double mathPi();
 //! \usage: ex4 <lowerbound> <upperbound>
 //e.g. 3..14
 int main(int argc, char** argv)
-{ 
+{
   double startTime=WallTime();
   double endTime=0.0;
   double S=pow(mathPi(),2)/6; //reference value S
@@ -33,11 +33,11 @@ int main(int argc, char** argv)
   int kupper=0;
   double Snpartial=0.0;
   /*---DECOMMENT FOR DEBUGGING PURPOSES---
-    fprintf(stdout, "------precalculated values------\n");
-    fprintf(stdout, "-- PI: %f\n", mathPi());
-    fprintf(stdout, "-- S: %f\n", S);
-    fprintf(stdout, "--------------------------------\n");
-  */
+fprintf(stdout, "------precalculated values------\n");
+fprintf(stdout, "-- PI: %f\n", mathPi());
+fprintf(stdout, "-- S: %f\n", S);
+fprintf(stdout, "--------------------------------\n");
+*/
 
   if(argc<3 || argc>3)
   {
@@ -50,54 +50,58 @@ int main(int argc, char** argv)
   
   int ktemp=klower;
   /*---DECOMMENT FOR DEBUGGING PURPOSES---
-    fprintf(stdout, "------command line arguments------\n");
-    fprintf(stdout, "-- lower bound: %d\n", klower);
-    fprintf(stdout, "-- upper bound: %d\n", kupper);
-    fprintf(stdout, "----------------------------------\n");
-  */
+fprintf(stdout, "------command line arguments------\n");
+fprintf(stdout, "-- lower bound: %d\n", klower);
+fprintf(stdout, "-- upper bound: %d\n", kupper);
+fprintf(stdout, "----------------------------------\n");
+*/
 
   int vectorlength=pow(2,kupper); //maximum vector length
   init_app(argc, argv, &rank, &size);
   Vector v=createVector(vectorlength); //storage vector for sum elements
 //calculate vector elements
-  #pragma omp parallel for schedule(guided,1) reduction(+:Sn)
+#pragma omp parallel for schedule(guided,1) reduction(+:Sn)
   for(i=1; i<=kupper; i++)
   {
     Snpartial=0.0;
- //   #pragma omp parallel for schedule(guided,1) reduction(+:Snpartial)
+ // #pragma omp parallel for schedule(guided,1) reduction(+:Snpartial)
     for(j=pow(2,i-1); j<pow(2,i); j++) //starting from element 1
     {
       //calculating j, storing in j-1
       //e.g. calculating 1st element, storing in data[0]
       //otherwise buffer overflow at last element
-      v->data[j-1]=1/pow(j,2); 
+      v->data[j-1]=1.0/pow(j,2);
       Snpartial+=v->data[j-1];
 
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---
-    fprintf(stdout, "---------sum calculation--------\n");
-    fprintf(stdout, "-- v->data[%d]: %f\n", j, v->data[j-1]);
-    fprintf(stdout, "-- Snpartial: %f\n", Snpartial);
-    fprintf(stdout, "--------------------------------\n");
-    if(i%10==0)
-    {
-      getchar();
-    }
-  */
+ /* ---DECOMMENT FOR DEBUGGING PURPOSES---*/
+fprintf(stdout, "---------sum calculation--------\n");
+fprintf(stdout, "-- v->data[%d]: %f\n", j, v->data[j-1]);
+fprintf(stdout, "-- Snpartial: %f\n", Snpartial);
+fprintf(stdout, "-- Sn: %f\n", Sn);
+fprintf(stdout, "--------------------------------\n");
+if(i%10==0)
+{
+getchar();
+}
+/**/
     }
     Sn += Snpartial;
     if(i>=klower && i<=kupper)
     {
       diff=S-Sn;
-      fprintf(stdout, "k=%d\n  elements:%d\n  %lf\n--------------------\n", i, j, diff);
+
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---*/
+fprintf(stdout, "-----------calculation----------\n");
+fprintf(stdout, "-- Sn: %f\n", Sn);
+fprintf(stdout, "-- S: %f\n", S);
+fprintf(stdout, "-- diff: %f\n", diff);
+fprintf(stdout, "--------------------------------\n");
+/**/
+
+      fprintf(stdout, "k=%d\n elements:%d\n S-Sn:%lf\n--------------------\n", i, j, diff);
     }
   }
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---
-    fprintf(stdout, "-----------calculation----------\n");
-    fprintf(stdout, "-- Sn: %f\n", Sn);
-    fprintf(stdout, "-- S: %f\n", S);
-    fprintf(stdout, "-- diff: %f\n", diff);
-    fprintf(stdout, "--------------------------------\n");
-  */
+
 
   endTime=WallTime();
 
--- TMA4280_Supercomputing/Exercise/ex4/src/ex4MPI.c
+++ TMA4280_Supercomputing/Exercise/ex4/src/ex4MPI.c
@@ -2,7 +2,7 @@
   * Author: Andreas J. Hoermer
   * Email: andreas (at) hoermer.at 
   * 
-  * Filename: ex4.c
+  * Filename: ex4MPI.c
   * LastChange: 04.02.2014
  **/
 
@@ -29,8 +29,7 @@ int main(int argc, char** argv)
   double diff=0.0; //difference S-Sn
   int rank=0;
   int size=0;
-  int klower=0;
-  int kupper=0;
+  int k=0;
   double Snpartial=0.0;
   /*---DECOMMENT FOR DEBUGGING PURPOSES---
     fprintf(stdout, "------precalculated values------\n");
@@ -39,68 +38,76 @@ int main(int argc, char** argv)
     fprintf(stdout, "--------------------------------\n");
   */
 
-  if(argc<3 || argc>3)
+  if(argc<2 || argc>2)
   {
-    fprintf(stdout, "\nusage: ex4 <lowerbound> <upperbound>\nexiting...\n\n");
+    fprintf(stdout, "\nusage: ex4 <k> \nexiting...\n\n");
     return EXIT_FAILURE;
   }
 
-  klower=atoi(argv[1]);
-  kupper=atoi(argv[2]);
+  k=atoi(argv[1]);
   
-  int ktemp=klower;
+  int ktemp=k;
   /*---DECOMMENT FOR DEBUGGING PURPOSES---
     fprintf(stdout, "------command line arguments------\n");
-    fprintf(stdout, "-- lower bound: %d\n", klower);
-    fprintf(stdout, "-- upper bound: %d\n", kupper);
+    fprintf(stdout, "-- k %d\n", k);
     fprintf(stdout, "----------------------------------\n");
   */
 
-  int vectorlength=pow(2,kupper); //maximum vector length
+  int vectorlength=pow(2,k); //maximum vector length
   init_app(argc, argv, &rank, &size);
+
+#ifdef HAVE_MPI
+
+#else
   Vector v=createVector(vectorlength); //storage vector for sum elements
-//calculate vector elements
-  #pragma omp parallel for schedule(guided,1) reduction(+:Sn)
-  for(i=1; i<=kupper; i++)
+#endif
+
+  if(rank==0)
   {
-    Snpartial=0.0;
- //   #pragma omp parallel for schedule(guided,1) reduction(+:Snpartial)
-    for(j=pow(2,i-1); j<pow(2,i); j++) //starting from element 1
-    {
+  //generating vector elements
+
+  //partitioning and distributing vector elements
+
+  //collecting and sum up
+
+  //printout
+  }
+  else
+  {
+  //calculate partial sums
+  
+  }
+
+  /*---DECOMMENT FOR DEBUGGING PURPOSES--- 
+    fprintf(stdout, "-- Vector length %d\n", vectorlength);
+    fprintf(stdout, "----------------------------------\n");
+  */
+
+//calculate vector elements
+ // #pragma omp parallel for schedule(guided,1) reduction(+:Sn)
+  for(i=1; i<=vectorlength; i++)
+  {
       //calculating j, storing in j-1
       //e.g. calculating 1st element, storing in data[0]
       //otherwise buffer overflow at last element
-      v->data[j-1]=1/pow(j,2); 
-      Snpartial+=v->data[j-1];
+      v->data[i-1]=1.0/pow(i,2); 
+      Sn+=v->data[i-1];
 
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---*/
     fprintf(stdout, "---------sum calculation--------\n");
     fprintf(stdout, "-- v->data[%d]: %f\n", j, v->data[j-1]);
-    fprintf(stdout, "-- Snpartial: %f\n", Snpartial);
+    fprintf(stdout, "-- Sn: %f\n", Sn);
     fprintf(stdout, "--------------------------------\n");
     if(i%10==0)
     {
       getchar();
     }
-  */
-    }
-    Sn += Snpartial;
-    if(i>=klower && i<=kupper)
-    {
-      diff=S-Sn;
-      fprintf(stdout, "k=%d\n  elements:%d\n  %lf\n--------------------\n", i, j, diff);
-    }
+ /* */
   }
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---
-    fprintf(stdout, "-----------calculation----------\n");
-    fprintf(stdout, "-- Sn: %f\n", Sn);
-    fprintf(stdout, "-- S: %f\n", S);
-    fprintf(stdout, "-- diff: %f\n", diff);
-    fprintf(stdout, "--------------------------------\n");
-  */
-
+  diff=S-Sn;
+  fprintf(stdout, "k=%d\n  elements:%d\n  %lf\n--------------------\n", k, i-1, diff);
+ 
   endTime=WallTime();
-
   fprintf(stdout, "total run time: %lf\n\n", endTime-startTime);
 
   freeVector(v);
