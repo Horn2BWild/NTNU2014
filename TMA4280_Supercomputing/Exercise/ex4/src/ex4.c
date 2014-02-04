6db3628da02b762e73afde7d9da5682672f1dfee
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-02-04 11:34:47 +0100

    added some debugging code

--- TMA4280_Supercomputing/Exercise/ex4/src/ex4.c
+++ TMA4280_Supercomputing/Exercise/ex4/src/ex4.c
@@ -15,19 +15,19 @@ int main(int argc, char** argv)
   int j=0;
   double diff=0.0;
 
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---*/
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---
   fprintf(stdout, "------precalculated values------\n");
   fprintf(stdout, "-- PI: %f\n", mathPi());
   fprintf(stdout, "-- S: %f\n", S);
   fprintf(stdout, "--------------------------------\n");
-/*  */
+  */
 
 //calculate vector elements
   for(i=1; i<vectorlength; i++)
   {
     v->data[i]=1/pow(i,2);
     Sn += v->data[i];
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---*/
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---
     fprintf(stdout, "---------sum calculation--------\n");
     fprintf(stdout, "-- v->data[%d]: %f\n", i, v->data[i]);
     fprintf(stdout, "-- Sn: %f\n", Sn);
@@ -36,20 +36,20 @@ int main(int argc, char** argv)
     {
       getchar();
     }
-/*  */
+  */
 
     for(j=3; j<=14; j++)
     {
       if(i==pow(2,j)-1)
       {
         diff=S-Sn;
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---*/
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---
         fprintf(stdout, "-----------calculation----------\n");
         fprintf(stdout, "-- Sn: %f\n", Sn);
         fprintf(stdout, "-- S: %f\n", S);
         fprintf(stdout, "-- diff: %f\n", diff);
         fprintf(stdout, "--------------------------------\n");
-/*  */
+  */
 
         fprintf(stdout, "k=%d\n  elements:%d\n  %lf\n--------------------\n", j, i+1, diff);
       }
