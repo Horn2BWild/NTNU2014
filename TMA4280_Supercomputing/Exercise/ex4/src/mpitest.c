1f65539cdb79af396a71a7bbd7c45daec4ab4559
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-02-05 12:57:35 +0100

    MPI_Scatter call

--- TMA4280_Supercomputing/Exercise/ex4/src/mpitest.c
+++ TMA4280_Supercomputing/Exercise/ex4/src/mpitest.c
@@ -12,35 +12,40 @@ int main(int argc, char** argv)
   MPI_Status status;
   char message[20];
   int j=0;
+  double sum=0;
   double* sendvec=(double*)malloc(VECTORSIZE*sizeof(double));
-  double* receivevec=(double*)malloc(VECTORSIZE*sizeof(double));
+  //double* receivevec=(double*)malloc(VECTORSIZE*sizeof(double));
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  double* receivevec=(double*)malloc(sizeof(double)*((VECTORSIZE/size)+1));
   tag = 100;
 
   if (rank == 0) 
   {
     for(i=0;i<VECTORSIZE;i++)
     {
-      vec[i]=1.0/pow((i+1),2);
+      sendvec[i]=i;
     }
-    for (i=1; i < size; ++i)
-      MPI_Send(vec, sizeof(vec), MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
+
+    MPI_Scatter(sendvec, sizeof(sendvec)/(size*sizeof(double)), MPI_DOUBLE, receivevec, sizeof(sendvec)/(size*sizeof(double)), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  //  for (i=1; i < size; ++i)
+  //    MPI_Send(vec, sizeof(vec), MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
   } 
   else
   {
-    MPI_Recv(vec, sizeof(vec), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
-    for(j=0;j<VECTORSIZE;j++)
+
+   // MPI_Recv(receivevec, sizeof(vec), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+    for(j=0;j<VECTORSIZE/size;j++)
     {
-      vec[j]+=rank;
+      sum+=receivevec[j];
     }
   }
 
-  for(i=0;i<VECTORSIZE;i++)
+  for(i=0;i<size;i++)
   {
-    printf("process %d: %f\n", rank, vec[i]);
+    printf("process %d: %f\n", i, sum);
   }
 
   MPI_Finalize();
