ae3986b3d5baec0adb5472aa2c1aa9cd119aef60
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-02-05 14:12:32 +0100

    MPI testfile working for MPI sum reduce

--- TMA4280_Supercomputing/Exercise/ex4/src/mpitest.c
+++ TMA4280_Supercomputing/Exercise/ex4/src/mpitest.c
@@ -17,10 +17,6 @@ int main(int argc, char** argv)
   int *displ, *sublength;
   double* globalsum=(double*)malloc(sizeof(double));
   double* sendvec=(double*)malloc(VECTORSIZE*sizeof(double));
-  //double* receivevec=(double*)malloc(VECTORSIZE*sizeof(double));
-  //MPI_Init(&argc, &argv);
-  //MPI_Comm_size(MPI_COMM_WORLD, &size);
-  //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   double* receivevec=(double*)malloc(sizeof(double)*((VECTORSIZE/size)+1));
   tag = 100;
@@ -34,45 +30,46 @@ int main(int argc, char** argv)
     {
       sendvec[i]=i;
     }
-  /*---DECOMMENT FOR DEBUGGING PURPOSES---*/
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---
     for(i=0; i<VECTORSIZE;i++)
     {
       fprintf(stdout, "process %d\n  sendvec[%d]=%f\n", rank, i, sendvec[i]);
     }
-  /**/
- //   MPI_Scatter(sendvec, VECTORSIZE/size, MPI_DOUBLE, receivevec, VECTORSIZE/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  */
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---
     for(i=0; i<size; i++)
     {
       fprintf(stdout, "size: %d, displacement %d\n", sublength[i], displ[i]);
       fprintf(stdout, "address %x\n", &(sendvec[displ[i]]));
     }
+  */
     for (i=0; i < size; ++i)
     {
-      fprintf(stdout, "---send for proc %d\n", i);
+      //fprintf(stdout, "---send for proc %d\n", i);
       double* vsend=&(sendvec[displ[i]]);
+  /*---DECOMMENT FOR DEBUGGING PURPOSES---
       for(j=0; j<sublength[i]; j++)
       {
         fprintf(stdout, "----process %d vsend[j]=%f\n", i, vsend[j]);
       }
+  */
       MPI_Send(vsend, sublength[i], MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
     }
   }
- // else
- // {
-	receivevec=(double*)malloc(sizeof(double)*sublength[rank]);
-    MPI_Recv(receivevec, sublength[rank], MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
-    fprintf(stdout, "process %d: data received\n", rank);
-    for(j=0;j<sublength[rank];j++)
-    {
-      fprintf(stdout, "process %d\n  element %d: %f\n  sum: %f\n", rank, j, receivevec[j], (*sum));
-      (*sum)+=receivevec[j];
-    }
+
+  receivevec=(double*)malloc(sizeof(double)*sublength[rank]);
+  MPI_Recv(receivevec, sublength[rank], MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+  fprintf(stdout, "process %d: data received\n", rank);
+  for(j=0;j<sublength[rank];j++)
+  {
+    fprintf(stdout, "process %d\n  element %d: %f\n  sum: %f\n", rank, j, receivevec[j], (*sum));
+    (*sum)+=receivevec[j];
+  }
   //}
-MPI_Reduce(sum, globalsum, sizeof(double), MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
+  //MPI_Reduce(sum, globalsum, sizeof(double), MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
+  MPI_Allreduce(sum, globalsum, sizeof(double), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-
-    printf("sum %f\n", *globalsum);
-
+  printf("sum %f\n", *globalsum);
 
   close_app();
   return 0;
