9210fe8ad7ea3a18411ce94cb8a842e13243ac36
Andreas J Hoermer <andreas.hoermer@gmx.at>
2014-03-10 12:05:11 +0100

    changed file formatting

--- TMA4280_Supercomputing/Exercise/ex6/src/poissonparallel.c
+++ TMA4280_Supercomputing/Exercise/ex6/src/poissonparallel.c
@@ -113,7 +113,7 @@ int main(int argc, char **argv)
     fprintf(stdout, "-outside while\n");
     fflush(stdout);
 #endif
-  //  fprintf(stdout, "step 1\n");
+    //  fprintf(stdout, "step 1\n");
     init_app(argc, argv, &rank, &size);
 
     //calculate partial vector sizes to send
@@ -126,27 +126,28 @@ int main(int argc, char **argv)
 //        scnt[i]=elementcount;
 //    }
 //    scnt[0]=m%size;
- //   displ[0]=0;
-splitVector(m, size, &scnt, &displ);
+//   displ[0]=0;
+    splitVector(m, size, &scnt, &displ);
 //    fprintf(stdout, "step 2\n");
 
     //calculate partial displacements to send
- //   int elementdisplacement=0;
- //   displ[0]=0;
- //   for(i=1; i<size; i++)
- //   {
- //       elementdisplacement+=scnt[i-1];
-  //      displ[i]=elementdisplacement;
- //   }
+//   int elementdisplacement=0;
+//   displ[0]=0;
+//   for(i=1; i<size; i++)
+//   {
+//       elementdisplacement+=scnt[i-1];
+    //      displ[i]=elementdisplacement;
+//   }
 
-if(rank==0){
-fprintf(stdout, "proc\tsize\tdispl\n");
-for(i=0; i<size; i++)
-{
-  fprintf(stdout, "%d\t%d\t%d\n", i, scnt[i], displ[i]);
-}
-fprintf(stdout, "-------------------\n");
-}
+    if(rank==0)
+    {
+        fprintf(stdout, "proc\tsize\tdispl\n");
+        for(i=0; i<size; i++)
+        {
+            fprintf(stdout, "%d\t%d\t%d\n", i, scnt[i], displ[i]);
+        }
+        fprintf(stdout, "-------------------\n");
+    }
     diag = createRealArray (m);
     b    = createReal2DArray (m,m);
     bt   = createReal2DArray (m,m);
@@ -154,14 +155,14 @@ fprintf(stdout, "-------------------\n");
 
     h    = 1./(Real)n;
     pi   = 4.*atan(1.);
- //   fprintf(stdout, "step 3\n");
+//   fprintf(stdout, "step 3\n");
 #pragma omp parallel for schedule(guided,1)
     for (i=0; i < m; i++)
     {
         diag[i] = 2.*(1.-cos((i+1)*pi/(Real)n));
     }
 
- //   fprintf(stdout, "step 4\n");
+//   fprintf(stdout, "step 4\n");
 #pragma omp parallel for schedule(guided,1)
     for (j=0; j < m; j++)
     {
@@ -170,158 +171,161 @@ fprintf(stdout, "-------------------\n");
             b[j][i] = h*h;
         }
     }
-   //     fprintf(stdout, "step 5\n");
+    //     fprintf(stdout, "step 5\n");
 #pragma omp parallel for schedule(guided,1)
     for (j=0; j < m; j++)
     {
         fst_(b[j], &n, z, &nn);
     }
-  //  fprintf(stdout, "step 6\n");
+    //  fprintf(stdout, "step 6\n");
 #if DEBUG_TESTMATRIX
-int value=0;
-for(i=0; i<m; i++)
-{
-  for(j=0; j<m; j++)
-  {
-    value++;
-    b[i][j]=value;
-  }
-}
-  //  fprintf(stdout, "step 7\n");
-if(rank==0){
-fprintf(stdout, "\n-----------------------\n");
-for(i=0; i<m; i++)
-{
-  for(j=0; j<m; j++)
-  {
-fprintf(stdout, "%.1f\t", b[i][j]);
-  }
-  fprintf(stdout, "\n");
-}
-fprintf(stdout, "-----------------------\n");
-}
+    int value=0;
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+            value++;
+            b[i][j]=value;
+        }
+    }
+    //  fprintf(stdout, "step 7\n");
+    if(rank==0)
+    {
+        fprintf(stdout, "\n-----------------------\n");
+        for(i=0; i<m; i++)
+        {
+            for(j=0; j<m; j++)
+            {
+                fprintf(stdout, "%.1f\t", b[i][j]);
+            }
+            fprintf(stdout, "\n");
+        }
+        fprintf(stdout, "-----------------------\n");
+    }
 #endif
-  //  fprintf(stdout, "step 8\n");
+    //  fprintf(stdout, "step 8\n");
 
 
-int proccnt=0;
-int rowcnt=0;
-int elementcnt=0;
-int columncnt=0;
-int vectorposition=0;
-Real *sendvector=malloc(m*scnt[rank]*sizeof(Real));
-Real *receivevector=malloc(m*scnt[rank]*sizeof(Real));
+    int proccnt=0;
+    int rowcnt=0;
+    int elementcnt=0;
+    int columncnt=0;
+    int vectorposition=0;
+    Real *sendvector=malloc(m*scnt[rank]*sizeof(Real));
+    Real *receivevector=malloc(m*scnt[rank]*sizeof(Real));
 
-fprintf(stdout, "size of vector on process %d: %d\n", rank, m*scnt[rank]);
+    fprintf(stdout, "size of vector on process %d: %d\n", rank, m*scnt[rank]);
 
-  for(rowcnt=0; rowcnt<m; rowcnt++)
-  {
+    for(rowcnt=0; rowcnt<m; rowcnt++)
+    {
 //for(proccnt=0; proccnt<size; proccnt++)
 //{
-    for(elementcnt=displ[rank]; elementcnt<displ[rank]+scnt[rank]; elementcnt++)
-    {
+        for(elementcnt=displ[rank]; elementcnt<displ[rank]+scnt[rank]; elementcnt++)
+        {
 
 //fprintf(stdout, "row: %d element %d rank %d: %f\n", rowcnt, elementcnt, rank, b[rowcnt][elementcnt]);
-      sendvector[vectorposition]=b[rowcnt][elementcnt];
-      vectorposition++;
+            sendvector[vectorposition]=b[rowcnt][elementcnt];
+            vectorposition++;
+        }
     }
-  }
 //}
-/*vectorposition=0;
-for(proccnt=0; proccnt<size; proccnt++)
-{
-for(columncnt=0; columncnt<scnt[rank]; columncnt++)
-{
-for(rowcnt=0; rowcnt<scnt[proccnt]; rowcnt++)
-{
-fprintf(stdout, "%.1f\t", sendvector[vectorposition]);
-vectorposition++;
-}
-fprintf(stdout, "\n");
-}
-}
-*/
+    /*vectorposition=0;
+    for(proccnt=0; proccnt<size; proccnt++)
+    {
+    for(columncnt=0; columncnt<scnt[rank]; columncnt++)
+    {
+    for(rowcnt=0; rowcnt<scnt[proccnt]; rowcnt++)
+    {
+    fprintf(stdout, "%.1f\t", sendvector[vectorposition]);
+    vectorposition++;
+    }
+    fprintf(stdout, "\n");
+    }
+    }
+    */
 
 
-fprintf(stdout, "---SENDVECTOR process %d---\n", rank);
-for(i=0; i<vectorposition; i++)
-{
-  fprintf(stdout, "%d.%d: %.1f\t",rank, i, sendvector[i]);
-}
-fprintf(stdout, "\n");
-
+    /*fprintf(stdout, "---SENDVECTOR process %d---\n", rank);
+    for(i=0; i<vectorposition; i++)
+    {
+      fprintf(stdout, "%d.%d: %.1f\t",rank, i, sendvector[i]);
+    }
+    fprintf(stdout, "\n");
+    */
     int *MPIdispl=malloc(size*sizeof(int));           // displacements
     int *MPIscnt=malloc(size*sizeof(int));            // send/receive count
 
-for(i=0; i<size; i++)
-{
-MPIdispl[i]=0;
-MPIscnt[i]=0;
-}
+    for(i=0; i<size; i++)
+    {
+        MPIdispl[i]=0;
+        MPIscnt[i]=0;
+    }
 
-for(i=1; i<size; i++)
-{
+    for(i=1; i<size; i++)
+    {
 //fprintf(stdout, "scnt[%d]: %d, scnt[%d]: %d\n", rank, scnt[rank], i, scnt[i]);
-MPIdispl[i]=MPIdispl[i-1]+scnt[rank]*scnt[i-1];
-}
+        MPIdispl[i]=MPIdispl[i-1]+scnt[rank]*scnt[i-1];
+    }
 
-for(i=0; i<size; i++)
-{
-fprintf(stdout, "rank %d: MPIdispl[%d]: %d\n", rank, i, MPIdispl[i]);
-}
-for(i=0; i<size; i++)
-{
-MPIscnt[i]=scnt[i]*scnt[rank];
-}
-for(i=0; i<size; i++)
-{
+    for(i=0; i<size; i++)
+    {
+        fprintf(stdout, "rank %d: MPIdispl[%d]: %d\n", rank, i, MPIdispl[i]);
+    }
+    for(i=0; i<size; i++)
+    {
+        MPIscnt[i]=scnt[i]*scnt[rank];
+    }
+    for(i=0; i<size; i++)
+    {
 //fprintf(stdout, "MPI p%d to %d: scnt: %d displ %d\n", i, rank, MPIscnt[i], MPIdispl[i]);
-}
+    }
 
-  MPI_Alltoallv (
-    &sendvector,	/* address of data to send  */
-		MPIscnt,	/* number of items to send to processes  */
-		MPIdispl, /* displacements for each process */
-		MPI_DOUBLE,	/* type of data  */
-		&receivevector,	/* address for receiving the data  */
-		/* NOTE: send data and receive data may NOT overlap */
-		MPIscnt,	/* number of items to receive
+    MPI_Alltoallv (
+        &sendvector,	/* address of data to send  */
+        MPIscnt,	/* number of items to send to processes  */
+        MPIdispl, /* displacements for each process */
+        MPI_DOUBLE,	/* type of data  */
+        &receivevector,	/* address for receiving the data  */
+        /* NOTE: send data and receive data may NOT overlap */
+        MPIscnt,	/* number of items to receive
 				   from any process  */
-    MPIdispl,
-		MPI_DOUBLE,	/* type of receive data  */
-		MPI_COMM_WORLD);
+        MPIdispl,
+        MPI_DOUBLE,	/* type of receive data  */
+        MPI_COMM_WORLD);
 
- //   transpose (bt,b,m);
+//   transpose (bt,b,m);
 
-   #if DEBUG_TESTMATRIX
-vectorposition=m*scnt[rank];
-fprintf(stdout, "receive vector size proc %d\n", vectorposition);
-fprintf(stdout, "---RECEIVEVECTOR process %d---\n", rank);
-for(i=0; i<vectorposition; i++)
-{
-  fprintf(stdout, "%d.%d: %.1f\t",rank, i, receivevector[i]);
-fflush(stdout);
-}
+#if DEBUG_TESTMATRIX
+    vectorposition=m*scnt[rank];
+    fprintf(stdout, "receive vector size proc %d\n", vectorposition);
+    fprintf(stdout, "---RECEIVEVECTOR process %d---\n", rank);
+    for(i=0; i<vectorposition; i++)
+    {
+        fprintf(stdout, "%d.%d: %.1f\t",rank, i, receivevector[i]);
+        fflush(stdout);
+    }
 
 #endif
- //   fprintf(stdout, "step 9\n");
-   for (i = 0; i <size; i++){
-      trans (&bt[displ[i]][0], scnt[i]);
-   }
+//   fprintf(stdout, "step 9\n");
+    for (i = 0; i <size; i++)
+    {
+        trans (&bt[displ[i]][0], scnt[i]);
+    }
 
-   #if DEBUG_TESTMATRIX
-   if(rank==0){
-   fprintf(stdout, "\n-----------------------\n");
-for(i=0; i<m; i++)
-{
-  for(j=0; j<m; j++)
-  {
-fprintf(stdout, "%.1f\t", bt[i][j]);
-  }
-  fprintf(stdout, "\n");
-}
-fprintf(stdout, "-----------------------\n");
-   }
+#if DEBUG_TESTMATRIX
+    if(rank==0)
+    {
+        fprintf(stdout, "\n-----------------------\n");
+        for(i=0; i<m; i++)
+        {
+            for(j=0; j<m; j++)
+            {
+                fprintf(stdout, "%.1f\t", bt[i][j]);
+            }
+            fprintf(stdout, "\n");
+        }
+        fprintf(stdout, "-----------------------\n");
+    }
 #endif
 //s    fprintf(stdout, "step 10\n");
 #pragma omp parallel for schedule(guided,1)
@@ -329,7 +333,7 @@ fprintf(stdout, "-----------------------\n");
     {
         fstinv_(bt[i], &n, z, &nn);
     }
- //   fprintf(stdout, "step 11\n");
+//   fprintf(stdout, "step 11\n");
 #pragma omp parallel for schedule(guided,1)
     for (j=0; j < m; j++)
     {
@@ -338,14 +342,14 @@ fprintf(stdout, "-----------------------\n");
             bt[j][i] = bt[j][i]/(diag[i]+diag[j]);
         }
     }
-   //     fprintf(stdout, "step 12\n");
+    //     fprintf(stdout, "step 12\n");
 #pragma omp parallel for schedule(guided,1)
     for (i=0; i < m; i++)
     {
         fst_(bt[i], &n, z, &nn);
     }
 
- //   fprintf(stdout, "step 13\n");
+//   fprintf(stdout, "step 13\n");
 
     transpose (b,bt,m);
 
@@ -354,7 +358,7 @@ fprintf(stdout, "-----------------------\n");
     {
         fstinv_(b[j], &n, z, &nn);
     }
-  //  fprintf(stdout, "step 14\n");
+    //  fprintf(stdout, "step 14\n");
     umax = 0.0;
 #pragma omp parallel for schedule(guided,1)
     for (j=0; j < m; j++)
@@ -364,7 +368,7 @@ fprintf(stdout, "-----------------------\n");
             if (b[j][i] > umax) umax = b[j][i];
         }
     }
- //       fprintf(stdout, "step 15\n");
+//       fprintf(stdout, "step 15\n");
     printf (" umax = %e \n",umax);
     close_app();
     return EXIT_SUCCESS;
@@ -416,23 +420,23 @@ void trans (double *a, int n)
 /* transpose square matrix a, dimension nxn */
 
 {
-  int i, j;
-  int ij, ji, l;
-  double tmp;
-  ij = 0;
-  l = -1;
-  for (i = 0; i < n; i++)
+    int i, j;
+    int ij, ji, l;
+    double tmp;
+    ij = 0;
+    l = -1;
+    for (i = 0; i < n; i++)
     {
-      l += n + 1;
-      ji = l;
-      ij += i + 1;
-      for (j = i+1; j < n; j++)
-	{
-	  tmp = a[ij];
-	  a[ij] = a[ji];
-	  a[ji] = tmp;
-	  ij++;
-	  ji += n;
-	}
+        l += n + 1;
+        ji = l;
+        ij += i + 1;
+        for (j = i+1; j < n; j++)
+        {
+            tmp = a[ij];
+            a[ij] = a[ji];
+            a[ji] = tmp;
+            ij++;
+            ji += n;
+        }
     }
 }
