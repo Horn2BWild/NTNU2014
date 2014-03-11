66dc15de300be11091e5f6c81c9d5e54f379fe0b
dtormoen <tormo020@umn.edu>
2014-03-11 23:42:19 +0100

    Multiple changes: -Data is now split between processes instead of all processes having all data -Changed transposeMPI() to handle data being split across processes -Wrote a basic function for printing matrices -Wrote a function to merge matrices that are split across processes -Disabled OpenMP because it was causing weird problems with the data in the matrices
    I compared the umax error with our own function to the output of the serial code with the same function and the errors were exactly the same and behaved as expected.

--- TMA4280_Supercomputing/Exercise/ex6/src/poissonparallel.c
+++ TMA4280_Supercomputing/Exercise/ex6/src/poissonparallel.c
@@ -37,6 +37,7 @@
 #define DEBUG_MPISUMMARY 0
 #define DEBUG_ALL 0
 #define DEBUG_MPIINFO 0
+#define DEBUG_TESTTRANSPOSE 0
 
 
 typedef double Real;
@@ -50,6 +51,46 @@ void fstinv_(Real *v, int *n, Real *w, int *nn);
 
 void trans (double *a, int n);
 
+
+void mergeAndPrintMpiMat(Real **b, int size, int rank, int m, int* scnt, int* displ);
+void printMat(Real **b, int m);
+
+void mergeAndPrintMpiMat(Real **b, int size, int rank, int m, int* scnt, int* displ)
+{
+    MPI_Status status;
+    int tag = 10;
+
+    MPI_Send(b[0], scnt[rank]*m, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
+        Real** test = createReal2DArray(m,m);
+
+        int i = 0;
+        for (i = 0; i < size; ++i)
+        {
+            MPI_Recv(test[displ[i]], scnt[i]*m, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status);
+        }
+        printMat(test, m);
+    }
+}
+
+void printMat(Real **b, int m)
+{
+    printf("\n-----------------------\n");
+    int i,j;
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < m; ++j)
+        {
+            printf("%f ", b[j][i]);
+        }
+        printf("\n");
+    }
+    printf("-----------------------\n");
+
+}
+
 void transposeMPI(Real **bt, Real**b, int m, int rank, int size, int* scnt, int* displ)
 {
     int i=0;
@@ -61,27 +102,13 @@ void transposeMPI(Real **bt, Real**b, int m, int rank, int size, int* scnt, int*
     Real *sendvector=malloc(m*scnt[rank]*sizeof(Real));
     Real *receivevector=malloc(m*scnt[rank]*sizeof(Real));
 
- //   fprintf(stdout, "size of vector on process %d: %d\n", rank, m*scnt[rank]);
-
-
-
-  for(proccnt=0; proccnt<size; proccnt++)
-{
-  for(columncnt=displ[rank]; columncnt<scnt[rank]+displ[rank]; columncnt++)
-{
-//  fprintf(stdout, "rank %d columncnt %d proccnt %d\n", rank, columncnt, proccnt);
-    for(rowcnt=displ[proccnt]; rowcnt<displ[proccnt]+scnt[proccnt]; rowcnt++)
+    for(rowcnt=0; rowcnt<m; rowcnt++)
     {
-  //    fprintf(stdout, "rank: %d - columncnt: %d rowcnt: %d -- value: %f\n", rank, columncnt, rowcnt,b[columncnt][rowcnt]);
-      fflush(stdout);
-//for(proccnt=0; proccnt<size; proccnt++)
-//{
-
-//fprintf(stdout, "row: %d element %d rank %d: %f\n", rowcnt, elementcnt, rank, b[rowcnt][elementcnt]);
-            sendvector[vectorposition]=b[rowcnt][columncnt];
+        for(columncnt=0; columncnt<scnt[rank]; columncnt++)
+        {
+            sendvector[vectorposition]=b[columncnt][rowcnt];
             vectorposition++;
         }
-    }
     }
 
 
@@ -100,9 +127,9 @@ void transposeMPI(Real **bt, Real**b, int m, int rank, int size, int* scnt, int*
 //fprintf(stdout, "scnt[%d]: %d, scnt[%d]: %d\n", rank, scnt[rank], i, scnt[i]);
         MPIdispl[i]=MPIdispl[i-1]+scnt[rank]*scnt[i-1];
     }
-            for(i=0; i<size; i++)
+    for(i=0; i<size; i++)
     {
-              MPIscnt[i]=scnt[i]*scnt[rank];
+        MPIscnt[i]=scnt[i]*scnt[rank];
     }
 #if DEBUG_MPIINFO
     for(i=0; i<size; i++)
@@ -156,8 +183,6 @@ fflush(stdout);
         MPI_DOUBLE,	/* type of receive data  */
         MPI_COMM_WORLD);
 
-//   transpose (bt,b,m);
-
 #if DEBUG_TESTMATRIX
   //  vectorposition=m*scnt[rank];
     fprintf(stdout, "receive vector size proc %d: %d\n", rank, vectorposition);
@@ -171,19 +196,20 @@ fflush(stdout);
 #endif
 //   fprintf(stdout, "proc %d step 9\n", rank);
 
-vectorposition=0;
-for(rowcnt=0; rowcnt<m; rowcnt++)
-{
-  for(columncnt=0; columncnt<scnt[rank];columncnt++)
-  {
-    bt[columncnt][rowcnt]=receivevector[vectorposition];
-    vectorposition++;
-   // fprintf(stdout, "%f\t", bt[columncnt][rowcnt]);
-  }
- // fprintf(stdout,"\n");
-}
-
-
+    vectorposition = 0;
+    for (proccnt = 0; proccnt < size; ++proccnt)
+    {
+        for(columncnt=0; columncnt<scnt[rank];columncnt++)
+        {
+            for (rowcnt = displ[proccnt]; rowcnt < displ[proccnt] + scnt[proccnt]; ++rowcnt)
+            {
+            
+                bt[columncnt][rowcnt]=receivevector[vectorposition];
+                vectorposition++;
+               // fprintf(stdout, "%f\t", bt[columncnt][rowcnt]);
+            }
+        }
+    }
 }
 
 
@@ -268,31 +294,50 @@ int main(int argc, char **argv)
         fprintf(stdout, "-------------------\n");
     }
     diag = createRealArray (m);
-    b    = createReal2DArray (m,m);
-    bt   = createReal2DArray (m,m);
+    b    = createReal2DArray (scnt[rank],m);
+    bt   = createReal2DArray (scnt[rank],m);
     z    = createRealArray (nn);
 
     h    = 1./(Real)n;
     pi   = 4.*atan(1.);
 //   fprintf(stdout, "step 3\n");
-#pragma omp parallel for schedule(guided,1)
+// #pragma omp parallel for schedule(guided,1)
     for (i=0; i < m; i++)
     {
-        diag[i] = 2.*(1.-cos((i+1)*pi/(Real)n));
+        diag[i] = 2.*(1.-cos((i+1) * pi/(Real)n));
     }
 
 //   fprintf(stdout, "step 4\n");
-#pragma omp parallel for schedule(guided,1)
-    for (j=0; j < m; j++)
+// #pragma omp parallel for schedule(guided,1)
+    for (j=0; j < scnt[rank]; j++)
     {
         for (i=0; i < m; i++)
         {
-            b[j][i] = h*h*5*M_PI*M_PI*(sin(M_PI*(j+1)*h)*sin(2*M_PI*(i+1)*h));
+            b[j][i] = h*h*5*M_PI*M_PI*(
+                sin(M_PI*(j+1+displ[rank])*h)*
+                sin(2*M_PI*(i+1)*h));
+#if DEBUG_TESTTRANSPOSE
+            b[j][i] = i*m + j + displ[rank];
+#endif
         }
     }
+
+#if DEBUG_TESTTRANSPOSE
+//********** testing stuff
+    mergeAndPrintMpiMat(b, size, rank, m, scnt, displ);
+    transposeMPI(bt,b,m,rank,size, scnt, displ);
+    mergeAndPrintMpiMat(bt, size, rank, m, scnt, displ);
+
+    close_app();
+    return EXIT_SUCCESS;
+//********** end testing stuff
+#endif
+
+
+
     //     fprintf(stdout, "step 5\n");
-#pragma omp parallel for schedule(guided,1)
-    for (j=0; j < m; j++)
+// #pragma omp parallel for schedule(guided,1)
+    for (j=0; j < scnt[rank]; j++)
     {
         fst_(b[j], &n, z, &nn);
     }
@@ -324,25 +369,27 @@ int main(int argc, char **argv)
 #endif
     //  fprintf(stdout, "step 8\n");
 
+    // mergeAndPrintMpiMat(b, size, rank, m, scnt, displ);
 transposeMPI(bt,b,m,rank,size, scnt, displ);
+    // mergeAndPrintMpiMat(bt, size, rank, m, scnt, displ);
 
-             #pragma omp parallel for schedule(guided,1)
-    for (i=0; i < m; i++)
+// #pragma omp parallel for schedule(guided,1)
+    for (i=0; i < scnt[rank]; i++)
     {
         fstinv_(bt[i], &n, z, &nn);
     }
  //  fprintf(stdout, "proc %d step 11\n", rank);
-#pragma omp parallel for schedule(guided,1)
-    for (j=0; j < m; j++)
+// #pragma omp parallel for schedule(guided,1)
+    for (j=0; j < scnt[rank]; j++)
     {
         for (i=0; i < m; i++)
         {
-            bt[j][i] = bt[j][i]/(diag[i]+diag[j]);
+            bt[j][i] = bt[j][i]/(diag[i]+diag[j+displ[rank]]);
         }
     }
    //      fprintf(stdout, "proc %d step 12\n", rank);
-#pragma omp parallel for schedule(guided,1)
-    for (i=0; i < m; i++)
+// #pragma omp parallel for schedule(guided,1)
+    for (i=0; i < scnt[rank]; i++)
     {
         fst_(bt[i], &n, z, &nn);
     }
@@ -351,17 +398,19 @@ transposeMPI(bt,b,m,rank,size, scnt, displ);
 
 transposeMPI(b,bt,m,rank,size, scnt, displ);
 
-#pragma omp parallel for schedule(guided,1)
-    for (j=0; j < m; j++)
+// #pragma omp parallel for schedule(guided,1)
+    for (j=0; j < scnt[rank]; j++)
     {
         fstinv_(b[j], &n, z, &nn);
     }
   //    fprintf(stdout, "proc %d step 14\n", rank);
     umax = 0.0;
-#pragma omp parallel for schedule(guided,1)
-    for (j=0; j < m; j++) {
+// #pragma omp parallel for schedule(guided,1)
+    for (j=0; j < scnt[rank]; j++) {
         for (i=0; i < m; i++) {
-            double value = fabs(b[j][i] - sin(M_PI * (j+1)*h)*sin(2*M_PI*(i+1)*h));
+            double value = fabs(b[j][i] - 
+                sin(M_PI * (j+1+displ[rank])*h)*
+                sin(2*M_PI*(i+1)*h));
             if (value > umax) umax = value;
         }
     }
@@ -377,7 +426,7 @@ transposeMPI(b,bt,m,rank,size, scnt, displ);
     if (rank == 0)
     {
         fprintf(stdout, "proc %d done...\n", rank);
-        printf (" umax = %e \n",globalMax);
+        printf (" umax = %f \n",globalMax);
     }
     close_app();
     return EXIT_SUCCESS;
