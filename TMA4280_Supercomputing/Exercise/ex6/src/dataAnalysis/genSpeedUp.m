573efd87aab6f5539e9085866f88b72e277df798
dtormoen <tormo020@umn.edu>
2014-04-04 11:51:46 +0200

    Generated a graph of the speedup.

--- TMA4280_Supercomputing/Exercise/ex6/src/dataAnalysis/genSpeedUp.m
+++ TMA4280_Supercomputing/Exercise/ex6/src/dataAnalysis/genSpeedUp.m
@@ -2,11 +2,11 @@ function [  ] = genSpeedUp( )
 %GENSPEEDUP Summary of this function goes here
 %   Detailed explanation goes here
 
-baseline = dlmread('varying.txt');
+baseline = dlmread('proc1Poisson.txt');
 bsize = baseline(:,3);
 btime = baseline(:,4);
 
-vary = dlmread('varyPoisson.txt');
+vary = dlmread('varying2.txt');
 vsize = vary(:,3);
 vtime = vary(:,4);
 vproc = vary(:,1);
@@ -15,29 +15,40 @@ vproc = vary(:,1);
 [uniqueProc,~,pInd] = unique(vproc);
 datalen = length(uniqueProc);
 data = zeros(datalen, length(uniqueSizes));
+datac = zeros(datalen, length(uniqueSizes));
 
-for i = 1:uniqueProc
+for i = 1:length(uniqueProc)
     loc = 1;
-    for j = 1:length(vtime)
-        if (pInd(j) == i)
-            data(i,loc) = vtime(j);
-            loc = loc + 1;
+    for k = 1:length(vsize)
+        for j = 1:length(vtime)
+            if (pInd(j) == i && sInd(j) == loc)
+                datac(i,loc) = datac(i,loc) + 1;
+                data(i,loc) = (data(i,loc) * (datac(i,loc)-1) + vtime(j))/datac(i,loc);
+            end
         end
+        loc = loc + 1;
     end
 end
 
 speedup = data';
 
 figure, hold, plot ([1;uniqueProc],[1;uniqueProc]);
-for i = 1:length(uniqueSizes)
-    plot([1;uniqueProc], [1,btime(i)./speedup(i,:)]);
-end
+% for i = 1:length(uniqueSizes)
+i= 1;
+plot([1;uniqueProc], [1,btime(1)./speedup(i,:)]);
+i= 2;
+plot([1;uniqueProc], [1,btime(5)./speedup(i,:)]);
+i= 3;
+plot([1;uniqueProc], [1,btime(9)./speedup(i,:)]);
+i= 4;
+plot([1;uniqueProc], [1,btime(13)./speedup(i,:)]);
+% end
 
 
-figure, hold, plot(uniqueSizes, btime);
-for i = 1:uniqueProc
-    plot(uniqueSizes, data(i,:));
-end
+% figure, hold, plot(uniqueSizes, btime);
+% for i = 1:uniqueProc
+%     plot(uniqueSizes, data(i,:));
+% end
 
 end
 
